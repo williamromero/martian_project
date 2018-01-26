e12b89c7d7801fc5d006ae6f10d8221b2196f76d
William Romero <williamromerovela@gmail.com>
2018-01-25 20:12:17 -0600

    Added header fixed transition

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  var header = document.querySelector('.mp-principal-header');
+  var list_logo_item = document.querySelector('.mp-nav-item-logo');
+  var list_item = document.querySelector('.mp-nav-item');
+  
+  if (window) {
+    window.addEventListener('scroll', function () {
+      if (window.pageYOffset >= 200) {
+        header.classList.add('mp-principal-header-scrolled');
+        list_logo_item.classList.add('mp-nav-item-logo-able');
+      }
+    });
+
+    window.addEventListener('scroll', function () {
+      if (window.pageYOffset == 0) {
+        header.classList.remove('mp-principal-header-scrolled');
+        list_logo_item.classList.remove('mp-nav-item-logo-able');
+      }
+    });
+
+  }
+
+});
