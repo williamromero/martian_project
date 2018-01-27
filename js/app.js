9f86b3f95c6bf038a8ea4e1ed9439cc0022a0282
William Romero <williamromerovela@gmail.com>
2018-01-26 23:03:58 -0600

    Add modifications on website

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
+  var responsive_btn = document.querySelector('.mp-menu-responsive-btn');
   var header = document.querySelector('.mp-principal-header');
   var list_logo_item = document.querySelector('.mp-nav-item-logo');
   var list_item = document.querySelector('.mp-nav-item');
+  var header_nav = document.querySelector('.mp-principal-header-nav');
   
   if (window) {
     window.addEventListener('scroll', function () {
@@ -18,7 +20,23 @@ $(document).ready(function() {
         // list_item.classList.add('')
       }
     });
-
   }
+
+  if(responsive_btn){
+    responsive_btn.addEventListener('click', () => {
+      header_nav.classList.toggle('mp-principal-header-nav-open');
+    });
+  }
+
+  $(function() {
+    $('.link-item').click(function() {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+      if (target.length) {
+        $('html,body').animate({ scrollTop: target.offset().top, behavior: 'smooth' }, 4000);
+        return false;
+      }
+    });
+  });  
 
 });
